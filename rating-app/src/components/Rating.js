56818488a881c780c548d0bd4307ebbff83504fd
bsihame <sihamebazi@pursuit.org>
2020-04-01 11:53:45 -0400

    add function

---
+++ rating-app/src/components/Rating.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import Start from './Start'
+
+import React, { Component } from 'react'
+
+class Rating extends Component {
+  static defaultProps = { max: 5 };
+  constructor(props) {
+    super(props);
+    this.state = {
+      dynamicValue: props.stars,
+      value: 0
+    }
+    This_meanings = {
+      0: "No Rating",
+      1: "Terrible",
+      2: "Mediocre",
+      3: "Average",
+      4: "Solid",
+      5: "Fantastic",
+    } 
+    
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Rating
+
+
+export default Start
